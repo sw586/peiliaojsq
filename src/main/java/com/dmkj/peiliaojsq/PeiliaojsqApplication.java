c98b5bbeea3a84597f6ef250a8bdf6b92bf36a7f
sw586 <sw586@126.com>
2024-10-24 16:57:30 +0800

    初始化，将https://start.spring.io/下载的项目包导入

---
+++ src/main/java/com/dmkj/peiliaojsq/PeiliaojsqApplication.java
@@ -0,0 +1,13 @@
+package com.dmkj.peiliaojsq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeiliaojsqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeiliaojsqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dmkj/peiliaojsq/PeiliaojsqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmkj.peiliaojsq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeiliaojsqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
